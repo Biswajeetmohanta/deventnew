e416b5fdb15bfcecae505969124203734f32c714
Biswajeetmohanta <rb486373@gmail.com>
2026-05-21 16:39:46 +0530

    remove filter option from case study

--- resources/views/case-studies/index.blade.php
+++ resources/views/case-studies/index.blade.php
@@ -27,6 +27,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             
             <!-- Dynamic Filters -->
+            {{--
             <div class="mb-12 flex flex-col gap-6">
                 <!-- Industry Filters -->
                 <div>
@@ -66,6 +67,7 @@
                     </div>
                 </div>
             </div>
+            --}}
 
             <!-- Case Studies Grid -->
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
